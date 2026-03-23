28d24f8d468105b77e9765cbde0273b970ec42a2
John Mistrinel <themistrinel@gmail.com>
2026-03-23 11:35:30 -0300

    feat: add EllenJoe animated cursor theme
    - Add converted Xcursor animated cursors from Windows .ani format
    - Update install.sh to copy cursors to ~/.icons
    - Configure Hyprland to use EllenJoe cursor theme

--- install.sh
+++ install.sh
@@ -89,6 +89,13 @@ cp -rf "$DOTFILES_DIR/fonts/"* "$FONT_DIR/"
 fc-cache -f
 print_success "Fontes instaladas."
 
+# --- Cursores ---
+print_status "Instalando cursores..."
+ICON_DIR="$HOME/.icons"
+mkdir -p "$ICON_DIR"
+cp -rf "$DOTFILES_DIR/cursors/"* "$ICON_DIR/"
+print_success "Cursores instalados."
+
 # --- Scripts executáveis ---
 find "$DOTFILES_DIR" -type f -name "*.sh" -exec chmod +x {} +
 
